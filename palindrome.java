e372a0eea8923d23547e6c6df0970394eed409c9
Nicholas Nikas <nicholas.nikas@outlook.com>
2020-02-07 20:30:02 -0500

    Create palindrome.java

---
+++ palindrome.java
@@ -0,0 +1,16 @@
+import java.util.Stack;
+import java.util.ArrayList;
+
+class Solution {
+    public boolean isPalindrome(int x) {
+       
+        String str = String.valueOf(x);
+        
+        for (int i=0; i<str.length(); i++){
+            if (str.charAt(i) != str.charAt((str.length()-1) - i)){
+                return false;
+            }
+        }
+        return true;
+    }
+}
